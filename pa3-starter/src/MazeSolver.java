a2e5f6b61d7de67803ce232b4046069630fbf4a2
Cheery <cheeryone821@gmail.com>
2021-04-14 22:04:49 -0700

    Good luck on PA3

---
+++ pa3-starter/src/MazeSolver.java
@@ -0,0 +1,11 @@
+/* Class to implement a Maze solver */
+
+public abstract class MazeSolver {
+	
+	public static Square solve(Maze maze, SearchWorklist wl) {
+		/* Complete this method */
+		return null;
+	}
+
+	/* Add any helper methods you want */
+}
---
+++ pa3-starter/src/TestSolvers.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+
+/*
+ * Write your JUnit tests here
+ * Use the formatMaze() method to get nicer JUnit output
+ */
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestSolvers {
+
+	/* Helper method to compare two mazes */
+	public void checkMaze(SearchWorklist wl, Maze startMaze, String[] expected) {
+		Square s = MazeSolver.solve(startMaze, wl);
+		if(expected == null) { assertNull(s); }
+		else {
+			ArrayList<Square> sp = startMaze.storePath();
+			String actualStr = formatMaze(startMaze.showSolution(sp));
+			String expectedStr = formatMaze(expected);
+			assertEquals(expectedStr, actualStr);
+		}
+	}	
+
+	/* Helper method to format String[] output as String */
+	public String formatMaze(String[] arr) {
+		String result = "";
+		for (String s: arr)
+			result += "\n"+s;
+		return (result+"\n");
+	}
+
+	/* Add your own Worklist tests below */
+
+	/* ************** HINT ******************** 
+	 * Use the helper methods to create simple
+	 * tests that are easier to debug. 
+	 */
+	
+	@Test
+	public void dummyTest() {
+		assertEquals("TODO: Remove this dummy test after writing your own tests.", "This is a dummy test.");
+	}
+	
+}
+
+
+
