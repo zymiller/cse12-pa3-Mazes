8608ef7fb70ba990daeaba4ffdb27e9e75d1ce40
zymiller <zymiller@ucsd.edu>
2023-04-24 15:14:48 -0700

    SearchWorkList completed

--- pa3-starter/src/SearchWorklist.java
+++ pa3-starter/src/SearchWorklist.java
@@ -2,13 +2,39 @@
  * Class to implement SearchWorklist as a Stack and a Queue.
  * You can use any built-in Java collections for this class.
  */
+import java.io.*;
+import java.util.*;
 
 class StackWorklist implements SearchWorklist {
+	Stack<Square> squareStack = new Stack<Square>();
 	
+	public void add(Square c) {
+		squareStack.push(c);
+	}
+
+	public Square remove() {
+		return squareStack.pop();
+	}
+
+	public boolean isEmpty() {
+		return squareStack.isEmpty();
+	}
 }
 
 class QueueWorklist implements SearchWorklist {
+	LinkedList<Square> squareList = new LinkedList<Square>();
 
+	public void add (Square c) {
+		squareList.add(c);
+	}
+
+	public Square remove() {
+		return squareList.removeFirst();
+	}
+
+	public boolean isEmpty() {
+		return squareList.size() == 0;
+	}
 }
 
 public interface SearchWorklist {
